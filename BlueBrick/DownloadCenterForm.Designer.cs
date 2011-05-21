e4d21f60a978bf387e4a179b09b94bd711cbcb8c
Alban Nanty <banban@banban.org>
2011-05-21 20:26:06 +0800

    | change the icon in all the forms to remove the white pixels visibles in Win7 | remove the icon in the default folder, use the one in the icons folder, and update it.

--- BlueBrick/DownloadCenterForm.Designer.cs
+++ BlueBrick/DownloadCenterForm.Designer.cs
@@ -28,129 +28,129 @@ namespace BlueBrick
 		/// </summary>
 		private void InitializeComponent()
 		{
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DownloadCenterForm));
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.DownloadListView = new System.Windows.Forms.ListView();
-            this.FileColumnHeader = new System.Windows.Forms.ColumnHeader();
-            this.DestinationColumnHeader = new System.Windows.Forms.ColumnHeader();
-            this.SourceColumnHeader = new System.Windows.Forms.ColumnHeader();
-            this.PercentColumnHeader = new System.Windows.Forms.ColumnHeader();
-            this.StartButton = new System.Windows.Forms.Button();
-            this.TotalProgressBar = new System.Windows.Forms.ProgressBar();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.closeButton = new System.Windows.Forms.Button();
-            this.cancelButton = new System.Windows.Forms.Button();
-            this.downloadBackgroundWorker = new System.ComponentModel.BackgroundWorker();
-            this.tableLayoutPanel1.SuspendLayout();
-            this.panel1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // tableLayoutPanel1
-            // 
-            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
-            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.DownloadListView, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.StartButton, 1, 2);
-            this.tableLayoutPanel1.Controls.Add(this.TotalProgressBar, 2, 2);
-            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 2);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            // 
-            // label1
-            // 
-            resources.ApplyResources(this.label1, "label1");
-            this.tableLayoutPanel1.SetColumnSpan(this.label1, 3);
-            this.label1.Name = "label1";
-            // 
-            // DownloadListView
-            // 
-            this.DownloadListView.AllowColumnReorder = true;
-            this.DownloadListView.AutoArrange = false;
-            this.DownloadListView.CheckBoxes = true;
-            this.DownloadListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DownloadCenterForm));
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.DownloadListView = new System.Windows.Forms.ListView();
+			this.FileColumnHeader = new System.Windows.Forms.ColumnHeader();
+			this.DestinationColumnHeader = new System.Windows.Forms.ColumnHeader();
+			this.SourceColumnHeader = new System.Windows.Forms.ColumnHeader();
+			this.PercentColumnHeader = new System.Windows.Forms.ColumnHeader();
+			this.StartButton = new System.Windows.Forms.Button();
+			this.TotalProgressBar = new System.Windows.Forms.ProgressBar();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.closeButton = new System.Windows.Forms.Button();
+			this.cancelButton = new System.Windows.Forms.Button();
+			this.downloadBackgroundWorker = new System.ComponentModel.BackgroundWorker();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+			this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.DownloadListView, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.StartButton, 1, 2);
+			this.tableLayoutPanel1.Controls.Add(this.TotalProgressBar, 2, 2);
+			this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 2);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			// 
+			// label1
+			// 
+			resources.ApplyResources(this.label1, "label1");
+			this.tableLayoutPanel1.SetColumnSpan(this.label1, 3);
+			this.label1.Name = "label1";
+			// 
+			// DownloadListView
+			// 
+			this.DownloadListView.AllowColumnReorder = true;
+			this.DownloadListView.AutoArrange = false;
+			this.DownloadListView.CheckBoxes = true;
+			this.DownloadListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.FileColumnHeader,
             this.DestinationColumnHeader,
             this.SourceColumnHeader,
             this.PercentColumnHeader});
-            this.tableLayoutPanel1.SetColumnSpan(this.DownloadListView, 3);
-            resources.ApplyResources(this.DownloadListView, "DownloadListView");
-            this.DownloadListView.FullRowSelect = true;
-            this.DownloadListView.LabelEdit = true;
-            this.DownloadListView.Name = "DownloadListView";
-            this.DownloadListView.UseCompatibleStateImageBehavior = false;
-            this.DownloadListView.View = System.Windows.Forms.View.Details;
-            this.DownloadListView.AfterLabelEdit += new System.Windows.Forms.LabelEditEventHandler(this.DownloadListView_AfterLabelEdit);
-            // 
-            // FileColumnHeader
-            // 
-            resources.ApplyResources(this.FileColumnHeader, "FileColumnHeader");
-            // 
-            // DestinationColumnHeader
-            // 
-            resources.ApplyResources(this.DestinationColumnHeader, "DestinationColumnHeader");
-            // 
-            // SourceColumnHeader
-            // 
-            resources.ApplyResources(this.SourceColumnHeader, "SourceColumnHeader");
-            // 
-            // PercentColumnHeader
-            // 
-            resources.ApplyResources(this.PercentColumnHeader, "PercentColumnHeader");
-            // 
-            // StartButton
-            // 
-            resources.ApplyResources(this.StartButton, "StartButton");
-            this.StartButton.Name = "StartButton";
-            this.StartButton.UseVisualStyleBackColor = true;
-            this.StartButton.Click += new System.EventHandler(this.StartStopButton_Click);
-            // 
-            // TotalProgressBar
-            // 
-            resources.ApplyResources(this.TotalProgressBar, "TotalProgressBar");
-            this.TotalProgressBar.Name = "TotalProgressBar";
-            this.TotalProgressBar.Step = 1;
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.closeButton);
-            this.panel1.Controls.Add(this.cancelButton);
-            resources.ApplyResources(this.panel1, "panel1");
-            this.panel1.Name = "panel1";
-            // 
-            // closeButton
-            // 
-            resources.ApplyResources(this.closeButton, "closeButton");
-            this.closeButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.closeButton.Name = "closeButton";
-            this.closeButton.UseVisualStyleBackColor = true;
-            // 
-            // cancelButton
-            // 
-            resources.ApplyResources(this.cancelButton, "cancelButton");
-            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Name = "cancelButton";
-            this.cancelButton.UseVisualStyleBackColor = true;
-            // 
-            // downloadBackgroundWorker
-            // 
-            this.downloadBackgroundWorker.WorkerReportsProgress = true;
-            this.downloadBackgroundWorker.WorkerSupportsCancellation = true;
-            this.downloadBackgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.downloadBackgroundWorker_DoWork);
-            this.downloadBackgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.downloadBackgroundWorker_RunWorkerCompleted);
-            this.downloadBackgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.downloadBackgroundWorker_ProgressChanged);
-            // 
-            // DownloadCenterForm
-            // 
-            this.AcceptButton = this.closeButton;
-            resources.ApplyResources(this, "$this");
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.tableLayoutPanel1);
-            this.Name = "DownloadCenterForm";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.DownloadCenterForm_FormClosing);
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
-            this.panel1.ResumeLayout(false);
-            this.ResumeLayout(false);
+			this.tableLayoutPanel1.SetColumnSpan(this.DownloadListView, 3);
+			resources.ApplyResources(this.DownloadListView, "DownloadListView");
+			this.DownloadListView.FullRowSelect = true;
+			this.DownloadListView.LabelEdit = true;
+			this.DownloadListView.Name = "DownloadListView";
+			this.DownloadListView.UseCompatibleStateImageBehavior = false;
+			this.DownloadListView.View = System.Windows.Forms.View.Details;
+			this.DownloadListView.AfterLabelEdit += new System.Windows.Forms.LabelEditEventHandler(this.DownloadListView_AfterLabelEdit);
+			// 
+			// FileColumnHeader
+			// 
+			resources.ApplyResources(this.FileColumnHeader, "FileColumnHeader");
+			// 
+			// DestinationColumnHeader
+			// 
+			resources.ApplyResources(this.DestinationColumnHeader, "DestinationColumnHeader");
+			// 
+			// SourceColumnHeader
+			// 
+			resources.ApplyResources(this.SourceColumnHeader, "SourceColumnHeader");
+			// 
+			// PercentColumnHeader
+			// 
+			resources.ApplyResources(this.PercentColumnHeader, "PercentColumnHeader");
+			// 
+			// StartButton
+			// 
+			resources.ApplyResources(this.StartButton, "StartButton");
+			this.StartButton.Name = "StartButton";
+			this.StartButton.UseVisualStyleBackColor = true;
+			this.StartButton.Click += new System.EventHandler(this.StartStopButton_Click);
+			// 
+			// TotalProgressBar
+			// 
+			resources.ApplyResources(this.TotalProgressBar, "TotalProgressBar");
+			this.TotalProgressBar.Name = "TotalProgressBar";
+			this.TotalProgressBar.Step = 1;
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.closeButton);
+			this.panel1.Controls.Add(this.cancelButton);
+			resources.ApplyResources(this.panel1, "panel1");
+			this.panel1.Name = "panel1";
+			// 
+			// closeButton
+			// 
+			resources.ApplyResources(this.closeButton, "closeButton");
+			this.closeButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.closeButton.Name = "closeButton";
+			this.closeButton.UseVisualStyleBackColor = true;
+			// 
+			// cancelButton
+			// 
+			resources.ApplyResources(this.cancelButton, "cancelButton");
+			this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.cancelButton.Name = "cancelButton";
+			this.cancelButton.UseVisualStyleBackColor = true;
+			// 
+			// downloadBackgroundWorker
+			// 
+			this.downloadBackgroundWorker.WorkerReportsProgress = true;
+			this.downloadBackgroundWorker.WorkerSupportsCancellation = true;
+			this.downloadBackgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.downloadBackgroundWorker_DoWork);
+			this.downloadBackgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.downloadBackgroundWorker_RunWorkerCompleted);
+			this.downloadBackgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.downloadBackgroundWorker_ProgressChanged);
+			// 
+			// DownloadCenterForm
+			// 
+			this.AcceptButton = this.closeButton;
+			resources.ApplyResources(this, "$this");
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Name = "DownloadCenterForm";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.DownloadCenterForm_FormClosing);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.panel1.ResumeLayout(false);
+			this.ResumeLayout(false);
 
 		}
 
