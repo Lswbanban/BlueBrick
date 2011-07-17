7f58d8b045503d699aeb8924692ffc17ba664aa4
Alban Nanty <banban@banban.org>
2011-07-17 17:04:47 +0800

    + add the loading of the HingAngle in the connectionType file | modify the FlexChain to take into account the hinge angle

--- BlueBrick/MapData/FlexTrack/FlexChain.cs
+++ BlueBrick/MapData/FlexTrack/FlexChain.cs
@@ -25,13 +25,14 @@ namespace BlueBrick.MapData.FlexTrack
 	class FlexChain
 	{
 		// data members
-		List<IKSolver.Bone_2D_CCD> mBoneList;
+		List<IKSolver.Bone_2D_CCD> mBoneList = null;
 
 		/// <summary>
 		/// Private constructor because there are condition to create it. Use static method createFlexChain instead.
 		/// </summary>
-		private FlexChain()
+		private FlexChain(int boneCount)
 		{
+			mBoneList = new List<IKSolver.Bone_2D_CCD>(boneCount);
 		}
 
 		/// <summary>
@@ -42,7 +43,7 @@ namespace BlueBrick.MapData.FlexTrack
 		/// </summary>
 		/// <param name="trackList">A set of track hopefully connected together, and hopefully containing flex track</param>
 		/// <param name="grabbedTrack">The part which will try to reach the target. It should be part of the list.</param>
-		public static FlexChain createFlexChain(List<LayerBrick.Brick> trackList, LayerBrick.Brick grabbedTrack)
+		public static FlexChain createFlexChain(List<Layer.LayerItem> trackList, LayerBrick.Brick grabbedTrack)
 		{
 			// if the grabbed part is not in the list, it failed
 			if (!trackList.Contains(grabbedTrack))
@@ -53,27 +54,37 @@ namespace BlueBrick.MapData.FlexTrack
 				return null;
 
 			// the chain to return
-			FlexChain flexChain = new FlexChain();
+			FlexChain flexChain = new FlexChain(trackList.Count);
 
 			// start to iterate from the grabbed track
 			LayerBrick.Brick currentTrack = grabbedTrack;
 			LayerBrick.Brick.ConnectionPoint currentConnection = grabbedTrack.ActiveConnectionPoint;
+			LayerBrick.Brick.ConnectionPoint hingedConnection = currentConnection;
 			// the chain is made with track that exclusively has 2 connections to make a chain.
 			while ((currentTrack != null) && (currentTrack.ConnectionPoints.Count == 2))
 			{
 				// get the other connection on the current brick
 				int nextIndex = (currentTrack.ConnectionPoints[0] == currentConnection) ? 1 : 0;
 				LayerBrick.Brick.ConnectionPoint nextConnection = currentTrack.ConnectionPoints[nextIndex];
+				LayerBrick.Brick nextTrack = nextConnection.ConnectedBrick;
 
-				// add the link in the list
-				IKSolver.Bone_2D_CCD newBone = new IKSolver.Bone_2D_CCD();
-				newBone.x = (currentConnection.PositionInStudWorldCoord.X - nextConnection.PositionInStudWorldCoord.X);
-				newBone.y = (currentConnection.PositionInStudWorldCoord.Y - nextConnection.PositionInStudWorldCoord.Y);
-				newBone.angle = 0;
-				flexChain.mBoneList.Add(newBone);
+				// check if the connection can rotate (if it is an hinge)
+				float hingeAngle = BrickLibrary.Instance.getConnexionHingeAngle(nextConnection.Type);
+				if ((hingeAngle != 0.0f) || (nextTrack == null) || (nextTrack.ConnectionPoints.Count == 2))
+				{
+					// add the link in the list
+					IKSolver.Bone_2D_CCD newBone = new IKSolver.Bone_2D_CCD();
+					newBone.x = (hingedConnection.PositionInStudWorldCoord.X - nextConnection.PositionInStudWorldCoord.X);
+					newBone.y = (hingedConnection.PositionInStudWorldCoord.Y - nextConnection.PositionInStudWorldCoord.Y);
+					newBone.angle = 0;
+					flexChain.mBoneList.Add(newBone);
+
+					// advance the current conncetion
+					hingedConnection = nextConnection.ConnectionLink;
+				}
 
 				// advance to the next link
-				currentTrack = nextConnection.ConnectedBrick;
+				currentTrack = nextTrack;
 				currentConnection = nextConnection.ConnectionLink;
 			}
 
