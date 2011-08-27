f880536becd377f48d1823ee3c3230b46b6a634d
Alban Nanty <Alban Nanty@localhost>
2011-08-27 09:10:33 +0800

    | transform the FlexChain into an Action and rename it FlexMove (the action code is not finished though) | add the Flex move action name in the loca kit

--- BlueBrick/Actions/Bricks/FlexMove.cs
+++ BlueBrick/Actions/Bricks/FlexMove.cs
@@ -16,15 +16,18 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Drawing;
+using BlueBrick.MapData;
+using BlueBrick.MapData.FlexTrack;
 
-namespace BlueBrick.MapData.FlexTrack
+namespace BlueBrick.Actions.Bricks
 {
 	/// <summary>
 	/// This class convert a set of track parts hopefully connected and containing flex tracks,
 	/// into a list of bones ready for the IKSolver.
 	/// </summary>
-	class FlexChain
+	class FlexMove : Action
 	{
+		#region sub class
 		// For optimization reason, we create a small class to store the brick connection and the angle 
 		// between the two bricks linked 
 		private class ChainLink
@@ -54,31 +57,110 @@ namespace BlueBrick.MapData.FlexTrack
 			}
 		}
 
-		// data members
+		/// <summary>
+		/// This class is used to record the initial and final state of all the bricks for the undo/redo purpose
+		/// </summary>
+		private class BrickTransform
+		{
+			public LayerBrick.Brick mBrick = null;
+			public PointF mPosition = PointF.Empty;
+			public float mOrientation = 0.0f;
+
+			public BrickTransform(LayerBrick.Brick brick, PointF position, float orientation)
+			{
+				mBrick = brick;
+				mPosition = position;
+				mOrientation = orientation;
+			}
+		}
+		#endregion
+
+		#region data members
+		// global data members
+		private bool mIsValid = false; // tell if this action can be a valid flex move action
+		private List<Layer.LayerItem> mBricksInTheFlexChain = null;
+
+		// data members for flex edition
 		private List<IKSolver.Bone_2D_CCD> mBoneList = null;
 		private List<ChainLink> mFlexChainList = null;
 		private int mRootHingedLinkIndex = 0; // the first hinged connection in the chain that can move
 		private float mInitialStaticCumulativeOrientation = 0.0f;
-		private List<Layer.LayerItem> mBricksInTheFlexChain = null;
+
+		// data members for action undo/redo purpose
+		private LayerBrick mBrickLayer = null;
+		private List<BrickTransform> mInitState = null;
+		private List<BrickTransform> mFinalState = null;
+		#endregion
 
 		#region get/set
+		public bool IsValid
+		{
+			get { return mIsValid; }
+		}
+
 		public List<Layer.LayerItem> BricksInTheFlexChain
 		{
 			get { return mBricksInTheFlexChain; }
 		}
 		#endregion
 
+		#region constructor
 		/// <summary>
-		/// Private constructor because there are condition to create it. Use static method createFlexChain instead.
+		/// The constructor should be called before starting to update the flex chain,
+		/// because the action save the initial state of the bricks in the constructor
+		/// This constructor Create a flex chain from a set of brick given a starting brick. From the best connection point
+		/// of this starting brick go through all the linked connection points while the brick only have
+		/// two connections. If the brick has only one or more than 2 connection (like a joint or crossing)
+		/// we stop the chain.
 		/// </summary>
-		private FlexChain(int boneCount)
+		/// <param name="layer">The layer on which the flex move is performed</param>
+		/// <param name="trackList">A set of track hopefully connected together, and hopefully containing flex track</param>
+		/// <param name="grabbedTrack">The part which will try to reach the target. It should be part of the list.</param>
+		/// <param name="mouseCoordInStud">the coordinate of the mouse in stud coord</param>
+		public FlexMove(LayerBrick layer, List<Layer.LayerItem> trackList, LayerBrick.Brick grabbedTrack, PointF mouseCoordInStud)
 		{
-			mBoneList = new List<IKSolver.Bone_2D_CCD>(boneCount);
-			mFlexChainList = new List<ChainLink>(boneCount * 2);
-			mBricksInTheFlexChain = new List<Layer.LayerItem>(boneCount);
+			// first we check if we can create a valid flex chain
+			mIsValid = false;
+			// check that the grabbed part is a part with 2 connection points
+			if (!grabbedTrack.HasConnectionPoint || grabbedTrack.ConnectionPoints.Count != 2)
+				return;
+
+			// if the grabbed part is not in the list, it failed
+			if (!trackList.Contains(grabbedTrack))
+				return;
+
+			// try to find the starting connection point, if the result is null, the flex chain cannot be created
+			LayerBrick.Brick.ConnectionPoint currentFirstConnection = findStartingConnectionPoint(trackList, grabbedTrack, mouseCoordInStud);
+			if (currentFirstConnection == null)
+				return;
+
+			// if we pass all the tests, the flex move is valid
+			mIsValid = true;
+
+			// init other data member
+			mBrickLayer = layer;
+			//create the edition data
+			ceateFlexChain(trackList, grabbedTrack, currentFirstConnection);
+			// and save the action data
+			recordState(ref mInitState);
 		}
 
-		private static float sSimplifyAngle(float angle)
+		/// <summary>
+		/// This method should be called only one time, when the flex chain has finished to be updated
+		/// and that the final state of the action is ready to be saved.
+		/// This method record the final state then undo all the move. The redo of the action will be created
+		/// as soon as the action is pushed into the action manager.
+		/// </summary>
+		public void finishActionConstruction()
+		{
+			recordState(ref mFinalState);
+			moveBricks(mInitState);
+		}
+
+		#endregion
+
+		#region flex move edition
+		private float simplifyAngle(float angle)
 		{
 			if (angle <= -360.0f)
 				angle += 360.0f;
@@ -87,14 +169,14 @@ namespace BlueBrick.MapData.FlexTrack
 			return angle;
 		}
 
-		private static void sAddNewBone(FlexChain flexChain, LayerBrick.Brick.ConnectionPoint connection, double maxAngleInDeg)
+		private void addNewBone(LayerBrick.Brick.ConnectionPoint connection, double maxAngleInDeg)
 		{
 			IKSolver.Bone_2D_CCD newBone = new IKSolver.Bone_2D_CCD();
 			newBone.maxAbsoluteAngleInRad = maxAngleInDeg * (Math.PI / 180);
 			newBone.worldX = connection.PositionInStudWorldCoord.X;
 			newBone.worldY = -connection.PositionInStudWorldCoord.Y; // BlueBrick use an indirect coord sys, and the IKSolver a direct one
 			newBone.connectionPoint = connection;
-			flexChain.mBoneList.Insert(0, newBone);
+			mBoneList.Insert(0, newBone);
 		}
 
 		/// <summary>
@@ -103,7 +185,7 @@ namespace BlueBrick.MapData.FlexTrack
 		/// </summary>
 		/// <param name="connectionLead">the current connection point, from which we need to find the next one</param>
 		/// <param name="isConnectionLeadingToFlexiblePart">a flag to tell if a flexible brick is found</param>
-		private static void sFollowConnectionToFindFlexiblePart(ref LayerBrick.Brick.ConnectionPoint connectionLead, ref bool isConnectionLeadingToFlexiblePart)
+		private void followConnectionToFindFlexiblePart(ref LayerBrick.Brick.ConnectionPoint connectionLead, ref bool isConnectionLeadingToFlexiblePart)
 		{
 			if (connectionLead != null)
 			{
@@ -152,7 +234,7 @@ namespace BlueBrick.MapData.FlexTrack
 		/// <param name="grabbedTrack">The part which will try to reach the target. It should be part of the list.</param>
 		/// <param name="mouseCoordInStud">the coordinate of the mouse in stud coord</param>
 		/// <returns>The best connection point of the grabbed part or null</returns>
-		private static LayerBrick.Brick.ConnectionPoint sFindStartingConnectionPoint(List<Layer.LayerItem> trackList, LayerBrick.Brick grabbedTrack, PointF mouseCoordInStud)
+		private LayerBrick.Brick.ConnectionPoint findStartingConnectionPoint(List<Layer.LayerItem> trackList, LayerBrick.Brick grabbedTrack, PointF mouseCoordInStud)
 		{
 			// try to find the best first connection point which will be the end target of the flex.
 			LayerBrick.Brick.ConnectionPoint grabbedFirstConnection = grabbedTrack.ConnectionPoints[0];
@@ -190,8 +272,8 @@ namespace BlueBrick.MapData.FlexTrack
 					remaingTrackToTestList.Remove(firstBrickLead);
 					remaingTrackToTestList.Remove(secondBrickLead);
 					// set the flag and follow the lead for the two leads
-					sFollowConnectionToFindFlexiblePart(ref firstConnectionLead, ref firstConnectionLeadToFlexiblePart);
-					sFollowConnectionToFindFlexiblePart(ref secondConnectionLead, ref secondConnectionLeadToFlexiblePart);
+					followConnectionToFindFlexiblePart(ref firstConnectionLead, ref firstConnectionLeadToFlexiblePart);
+					followConnectionToFindFlexiblePart(ref secondConnectionLead, ref secondConnectionLeadToFlexiblePart);
 				}
 				// either the loop is finished because we found flexible part on both side, or because we
 				// finished to explore the two paths. Check if we have only one side leading to a flexible part
@@ -234,29 +316,19 @@ namespace BlueBrick.MapData.FlexTrack
 		/// </summary>
 		/// <param name="trackList">A set of track hopefully connected together, and hopefully containing flex track</param>
 		/// <param name="grabbedTrack">The part which will try to reach the target. It should be part of the list.</param>
-		/// <param name="mouseCoordInStud">the coordinate of the mouse in stud coord</param>
-		public static FlexChain sCeateFlexChain(List<Layer.LayerItem> trackList, LayerBrick.Brick grabbedTrack, PointF mouseCoordInStud)
+		/// <param name="currentFirstConnection">The first connection from which to start, which is not null and belongs to the grabbed track</param>
+		public void ceateFlexChain(List<Layer.LayerItem> trackList, LayerBrick.Brick grabbedTrack, LayerBrick.Brick.ConnectionPoint currentFirstConnection)
 		{
-			// check that the grabbed part is a part with 2 connection points
-			if (!grabbedTrack.HasConnectionPoint || grabbedTrack.ConnectionPoints.Count != 2)
-				return null;
-
-			// if the grabbed part is not in the list, it failed
-			if (!trackList.Contains(grabbedTrack))
-				return null;
-
-			// try to find the starting connection point, if the result is null, the flex chain cannot be created
-			LayerBrick.Brick.ConnectionPoint currentFirstConnection = sFindStartingConnectionPoint(trackList, grabbedTrack, mouseCoordInStud);
-			if (currentFirstConnection == null)
-				return null;
-
-			// the chain to return
-			FlexChain flexChain = new FlexChain(trackList.Count);
+			// init all the arrays
+			int boneCount = trackList.Count;
+			mBoneList = new List<IKSolver.Bone_2D_CCD>(boneCount);
+			mFlexChainList = new List<ChainLink>(boneCount * 2);
+			mBricksInTheFlexChain = new List<Layer.LayerItem>(boneCount);
 
 			// start to iterate from the grabbed track
 			LayerBrick.Brick currentBrick = grabbedTrack;
 			ChainLink hingedLink = null;
-			sAddNewBone(flexChain, currentFirstConnection, 0.0);
+			addNewBone(currentFirstConnection, 0.0);
 			// the chain is made with track that exclusively has 2 connections to make a chain.
 			while ((currentBrick != null) && (currentBrick.ConnectionPoints.Count == 2) && trackList.Contains(currentBrick))
 			{
@@ -268,7 +340,7 @@ namespace BlueBrick.MapData.FlexTrack
 
 				// add the two connections of the brick
 				ChainLink link = new ChainLink(nextFirstConnection, currentSecondConnection);
-				flexChain.mFlexChainList.Insert(0, link);
+				mFlexChainList.Insert(0, link);
 
 				// check if the connection can rotate (if it is an hinge)
 				float hingeAngle = BrickLibrary.Instance.getConnexionHingeAngle(currentSecondConnection.Type);
@@ -277,14 +349,14 @@ namespace BlueBrick.MapData.FlexTrack
 					// advance the hinge conncetion
 					hingedLink = link;
 					// add the link in the list
-					sAddNewBone(flexChain, currentSecondConnection, hingeAngle);
+					addNewBone(currentSecondConnection, hingeAngle);
 					// compute the current angle between the hinge connection and set it to the current bone
 					// to do that we use the current angle between the connected brick and remove the static angle between them
 					// to only get the flexible angle.
 					float angleInDegree = 0.0f;
 					if (nextBrick != null)
-						angleInDegree = sSimplifyAngle(nextBrick.Orientation - currentBrick.Orientation - link.mAngleBetween);
-					flexChain.mBoneList[0].localAngleInRad = angleInDegree * (Math.PI / 180);
+						angleInDegree = simplifyAngle(nextBrick.Orientation - currentBrick.Orientation - link.mAngleBetween);
+					mBoneList[0].localAngleInRad = angleInDegree * (Math.PI / 180);
 
 					// save the initial static cumulative orientation: start with the orientation of the root brick
 					// if the hinge is connected to a brick, otherwise, if the hinge is free (connected to the world)
@@ -292,9 +364,9 @@ namespace BlueBrick.MapData.FlexTrack
 					// we set the value several time in the loop, in order to set it with the brick directly
 					// connected to the last hinge in the chain
 					if (nextBrick != null)
-						flexChain.mInitialStaticCumulativeOrientation = -nextBrick.Orientation;
+						mInitialStaticCumulativeOrientation = -nextBrick.Orientation;
 					else
-						flexChain.mInitialStaticCumulativeOrientation = -currentBrick.Orientation;
+						mInitialStaticCumulativeOrientation = -currentBrick.Orientation;
 				}
 
 				// advance to the next link
@@ -309,19 +381,17 @@ namespace BlueBrick.MapData.FlexTrack
 
 			// store the root hinge connection index (the last hinge found is the flexible root)
 			if (hingedLink != null)
-				flexChain.mRootHingedLinkIndex = flexChain.mFlexChainList.IndexOf(hingedLink);
+				mRootHingedLinkIndex = mFlexChainList.IndexOf(hingedLink);
 
 			// add the current brick in the list of bricks, by not going further than the root brick
-			LayerBrick.Brick.ConnectionPoint rootConnection = flexChain.mFlexChainList[flexChain.mRootHingedLinkIndex].mFirstConnection;
+			LayerBrick.Brick.ConnectionPoint rootConnection = mFlexChainList[mRootHingedLinkIndex].mFirstConnection;
 			if (rootConnection.mMyBrick != null)
-				flexChain.mBricksInTheFlexChain.Add(rootConnection.mMyBrick);
-			for (int i = flexChain.mRootHingedLinkIndex; i < flexChain.mFlexChainList.Count; ++i)
-				flexChain.mBricksInTheFlexChain.Add(flexChain.mFlexChainList[i].mSecondConnection.mMyBrick);
-
-			// return the chain
-			return flexChain;
+				mBricksInTheFlexChain.Add(rootConnection.mMyBrick);
+			for (int i = mRootHingedLinkIndex; i < mFlexChainList.Count; ++i)
+				mBricksInTheFlexChain.Add(mFlexChainList[i].mSecondConnection.mMyBrick);
 		}
 
+		#region update
 		/// <summary>
 		/// Try to reach the specify target position with this chain
 		/// </summary>
@@ -403,5 +473,38 @@ namespace BlueBrick.MapData.FlexTrack
 				mBoneList[lastIndex].worldY = -lastPosition.Y; // BlueBrick use an indirect coord sys, and the IKSolver a direct one
 			}
 		}
+		#endregion
+		#endregion
+
+		#region	Action method
+		public override string getName()
+		{
+			return BlueBrick.Properties.Resources.ActionFlexMove;
+		}
+
+		private void recordState(ref List<BrickTransform> state)
+		{
+			state = new List<BrickTransform>(mBricksInTheFlexChain.Count);
+		}
+
+		public override void redo()
+		{
+			moveBricks(mFinalState);
+		}
+
+		public override void undo()
+		{
+			moveBricks(mInitState);
+		}
+
+		private void moveBricks(List<BrickTransform> state)
+		{
+			foreach (BrickTransform transform in state)
+			{
+				transform.mBrick.Orientation = transform.mOrientation;
+				transform.mBrick.Position = transform.mPosition;
+			}
+		}
+		#endregion
 	}
 }
