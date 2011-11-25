ac1dcb1c860981602cd8a3c132cabd3b4c0a926d
Alban Nanty <banban@banban.org>
2011-11-25 19:24:35 +0800

    | Modify the design of the LoadErrorForm to make it work under Mono (replace the TableLayoutPanel by a SplitContainer)

--- BlueBrick/LoadErrorForm.cs
+++ BlueBrick/LoadErrorForm.cs
@@ -14,20 +14,37 @@ namespace BlueBrick
 		{
 			InitializeComponent();
 			// set the message and detail texts
-			this.MessageLabel.Text = message;
+			this.MessageTextBox.Text = message;
 			this.DetailsTextBox.Text = details;
 			// set the title
 			this.Text = title;
-			this.DetailButton.Text = Properties.Resources.ShowDetails;
+			// hide the detail box by default
+			showHideDetails(false);
 		}
 
 		private void DetailButton_Click(object sender, EventArgs e)
 		{
-			this.DetailsTextBox.Visible = !(this.DetailsTextBox.Visible);
-			if (this.DetailsTextBox.Visible)
+			// revert the status based on the status of the detail box
+			showHideDetails(!this.DetailsTextBox.Visible);
+		}
+
+		private void showHideDetails(bool isVisible)
+		{
+			// show or hide the slip container and the detail box
+			this.splitContainer2.Panel2Collapsed = !isVisible;
+			this.DetailsTextBox.Visible = isVisible;
+			// if the split container is visible add some border
+			// and change the text of the button
+			if (isVisible)
+			{
+				this.splitContainer2.BorderStyle = BorderStyle.FixedSingle;
 				this.DetailButton.Text = Properties.Resources.HideDetails;
+			}
 			else
+			{
+				this.splitContainer2.BorderStyle = BorderStyle.None;
 				this.DetailButton.Text = Properties.Resources.ShowDetails;
+			}
 		}
 
 		private void OkButton_Click(object sender, EventArgs e)
