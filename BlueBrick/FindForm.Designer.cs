847c2dfdfe836deafca15d29ac1a0976b212ce13
Alban Nanty <banban@banban.org>
2012-02-13 21:51:52 +0800

    + Add the Italian translation + Add the name of the new Italian translators in the About Box + Add a test plan excel file

--- BlueBrick/FindForm.Designer.cs
+++ BlueBrick/FindForm.Designer.cs
@@ -52,88 +52,132 @@ namespace BlueBrick
 			// 
 			// groupBox1
 			// 
+			this.groupBox1.AccessibleDescription = null;
+			this.groupBox1.AccessibleName = null;
+			resources.ApplyResources(this.groupBox1, "groupBox1");
+			this.groupBox1.BackgroundImage = null;
 			this.groupBox1.Controls.Add(this.FindPictureBox);
 			this.groupBox1.Controls.Add(this.ReplacePictureBox);
 			this.groupBox1.Controls.Add(this.ReplaceComboBox);
 			this.groupBox1.Controls.Add(this.label2);
 			this.groupBox1.Controls.Add(this.FindComboBox);
 			this.groupBox1.Controls.Add(this.label1);
-			resources.ApplyResources(this.groupBox1, "groupBox1");
+			this.groupBox1.Font = null;
 			this.groupBox1.Name = "groupBox1";
 			this.groupBox1.TabStop = false;
 			// 
 			// FindPictureBox
 			// 
+			this.FindPictureBox.AccessibleDescription = null;
+			this.FindPictureBox.AccessibleName = null;
 			resources.ApplyResources(this.FindPictureBox, "FindPictureBox");
+			this.FindPictureBox.BackgroundImage = null;
+			this.FindPictureBox.Font = null;
+			this.FindPictureBox.ImageLocation = null;
 			this.FindPictureBox.Name = "FindPictureBox";
 			this.FindPictureBox.TabStop = false;
 			// 
 			// ReplacePictureBox
 			// 
+			this.ReplacePictureBox.AccessibleDescription = null;
+			this.ReplacePictureBox.AccessibleName = null;
 			resources.ApplyResources(this.ReplacePictureBox, "ReplacePictureBox");
+			this.ReplacePictureBox.BackgroundImage = null;
+			this.ReplacePictureBox.Font = null;
+			this.ReplacePictureBox.ImageLocation = null;
 			this.ReplacePictureBox.Name = "ReplacePictureBox";
 			this.ReplacePictureBox.TabStop = false;
 			// 
 			// ReplaceComboBox
 			// 
+			this.ReplaceComboBox.AccessibleDescription = null;
+			this.ReplaceComboBox.AccessibleName = null;
+			resources.ApplyResources(this.ReplaceComboBox, "ReplaceComboBox");
 			this.ReplaceComboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
 			this.ReplaceComboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+			this.ReplaceComboBox.BackgroundImage = null;
 			this.ReplaceComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.ReplaceComboBox.Font = null;
 			this.ReplaceComboBox.FormattingEnabled = true;
-			resources.ApplyResources(this.ReplaceComboBox, "ReplaceComboBox");
 			this.ReplaceComboBox.Name = "ReplaceComboBox";
 			this.ReplaceComboBox.Sorted = true;
 			this.ReplaceComboBox.SelectedIndexChanged += new System.EventHandler(this.ReplaceComboBox_SelectedIndexChanged);
 			// 
 			// label2
 			// 
+			this.label2.AccessibleDescription = null;
+			this.label2.AccessibleName = null;
 			resources.ApplyResources(this.label2, "label2");
+			this.label2.Font = null;
 			this.label2.Name = "label2";
 			// 
 			// FindComboBox
 			// 
+			this.FindComboBox.AccessibleDescription = null;
+			this.FindComboBox.AccessibleName = null;
+			resources.ApplyResources(this.FindComboBox, "FindComboBox");
 			this.FindComboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
 			this.FindComboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+			this.FindComboBox.BackgroundImage = null;
 			this.FindComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.FindComboBox.Font = null;
 			this.FindComboBox.FormattingEnabled = true;
-			resources.ApplyResources(this.FindComboBox, "FindComboBox");
 			this.FindComboBox.Name = "FindComboBox";
 			this.FindComboBox.Sorted = true;
 			this.FindComboBox.SelectedIndexChanged += new System.EventHandler(this.FindComboBox_SelectedIndexChanged);
 			// 
 			// label1
 			// 
+			this.label1.AccessibleDescription = null;
+			this.label1.AccessibleName = null;
 			resources.ApplyResources(this.label1, "label1");
+			this.label1.Font = null;
 			this.label1.Name = "label1";
 			// 
 			// groupBox2
 			// 
+			this.groupBox2.AccessibleDescription = null;
+			this.groupBox2.AccessibleName = null;
+			resources.ApplyResources(this.groupBox2, "groupBox2");
+			this.groupBox2.BackgroundImage = null;
 			this.groupBox2.Controls.Add(this.allLayerCheckBox);
 			this.groupBox2.Controls.Add(this.LayerCheckedListBox);
 			this.groupBox2.Controls.Add(this.inLayerRadioButton);
 			this.groupBox2.Controls.Add(this.inCurrentSelectionRadioButton);
-			resources.ApplyResources(this.groupBox2, "groupBox2");
+			this.groupBox2.Font = null;
 			this.groupBox2.Name = "groupBox2";
 			this.groupBox2.TabStop = false;
 			// 
 			// allLayerCheckBox
 			// 
+			this.allLayerCheckBox.AccessibleDescription = null;
+			this.allLayerCheckBox.AccessibleName = null;
 			resources.ApplyResources(this.allLayerCheckBox, "allLayerCheckBox");
+			this.allLayerCheckBox.BackgroundImage = null;
+			this.allLayerCheckBox.Font = null;
 			this.allLayerCheckBox.Name = "allLayerCheckBox";
 			this.allLayerCheckBox.UseVisualStyleBackColor = true;
 			this.allLayerCheckBox.CheckedChanged += new System.EventHandler(this.allLayerCheckBox_CheckedChanged);
 			// 
 			// LayerCheckedListBox
 			// 
-			this.LayerCheckedListBox.CheckOnClick = true;
-			this.LayerCheckedListBox.FormattingEnabled = true;
+			this.LayerCheckedListBox.AccessibleDescription = null;
+			this.LayerCheckedListBox.AccessibleName = null;
 			resources.ApplyResources(this.LayerCheckedListBox, "LayerCheckedListBox");
+			this.LayerCheckedListBox.BackgroundImage = null;
+			this.LayerCheckedListBox.CheckOnClick = true;
+			this.LayerCheckedListBox.Font = null;
+			this.LayerCheckedListBox.FormattingEnabled = true;
 			this.LayerCheckedListBox.Name = "LayerCheckedListBox";
 			this.LayerCheckedListBox.SelectedIndexChanged += new System.EventHandler(this.LayerCheckedListBox_SelectedIndexChanged);
 			// 
 			// inLayerRadioButton
 			// 
+			this.inLayerRadioButton.AccessibleDescription = null;
+			this.inLayerRadioButton.AccessibleName = null;
 			resources.ApplyResources(this.inLayerRadioButton, "inLayerRadioButton");
+			this.inLayerRadioButton.BackgroundImage = null;
+			this.inLayerRadioButton.Font = null;
 			this.inLayerRadioButton.Name = "inLayerRadioButton";
 			this.inLayerRadioButton.TabStop = true;
 			this.inLayerRadioButton.UseVisualStyleBackColor = true;
@@ -141,42 +185,62 @@ namespace BlueBrick
 			// 
 			// inCurrentSelectionRadioButton
 			// 
+			this.inCurrentSelectionRadioButton.AccessibleDescription = null;
+			this.inCurrentSelectionRadioButton.AccessibleName = null;
 			resources.ApplyResources(this.inCurrentSelectionRadioButton, "inCurrentSelectionRadioButton");
+			this.inCurrentSelectionRadioButton.BackgroundImage = null;
+			this.inCurrentSelectionRadioButton.Font = null;
 			this.inCurrentSelectionRadioButton.Name = "inCurrentSelectionRadioButton";
 			this.inCurrentSelectionRadioButton.TabStop = true;
 			this.inCurrentSelectionRadioButton.UseVisualStyleBackColor = true;
 			// 
 			// cancelButton
 			// 
-			this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.cancelButton.AccessibleDescription = null;
+			this.cancelButton.AccessibleName = null;
 			resources.ApplyResources(this.cancelButton, "cancelButton");
+			this.cancelButton.BackgroundImage = null;
+			this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.cancelButton.Font = null;
 			this.cancelButton.Name = "cancelButton";
 			this.cancelButton.UseVisualStyleBackColor = true;
 			// 
 			// ReplaceButton
 			// 
+			this.ReplaceButton.AccessibleDescription = null;
+			this.ReplaceButton.AccessibleName = null;
 			resources.ApplyResources(this.ReplaceButton, "ReplaceButton");
+			this.ReplaceButton.BackgroundImage = null;
+			this.ReplaceButton.Font = null;
 			this.ReplaceButton.Name = "ReplaceButton";
 			this.ReplaceButton.UseVisualStyleBackColor = true;
 			this.ReplaceButton.Click += new System.EventHandler(this.ReplaceButton_Click);
 			// 
 			// SelectAllButton
 			// 
+			this.SelectAllButton.AccessibleDescription = null;
+			this.SelectAllButton.AccessibleName = null;
 			resources.ApplyResources(this.SelectAllButton, "SelectAllButton");
+			this.SelectAllButton.BackgroundImage = null;
+			this.SelectAllButton.Font = null;
 			this.SelectAllButton.Name = "SelectAllButton";
 			this.SelectAllButton.UseVisualStyleBackColor = true;
 			this.SelectAllButton.Click += new System.EventHandler(this.SelectAllButton_Click);
 			// 
 			// FindForm
 			// 
+			this.AccessibleDescription = null;
+			this.AccessibleName = null;
 			resources.ApplyResources(this, "$this");
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = null;
 			this.Controls.Add(this.SelectAllButton);
 			this.Controls.Add(this.ReplaceButton);
 			this.Controls.Add(this.cancelButton);
 			this.Controls.Add(this.groupBox2);
 			this.Controls.Add(this.groupBox1);
 			this.DoubleBuffered = true;
+			this.Font = null;
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
 			this.MaximizeBox = false;
 			this.Name = "FindForm";
