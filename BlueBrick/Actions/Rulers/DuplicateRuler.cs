704e1a32ac1f6a93d95ed0b75ab318d34c2ed7f6
Alban Nanty <Alban Nanty@localhost>
2013-03-03 00:30:54 +0800

    + add the delete and cut of Rulers (refactor the code in the MainForm and use the existing DeleteRuler action) + add the DuplicateRuler action (but not use it yet)

--- BlueBrick/Actions/Rulers/DuplicateRuler.cs
+++ BlueBrick/Actions/Rulers/DuplicateRuler.cs
@@ -1,4 +1,4 @@
-// BlueBrick, a LEGO(c) layout editor.
+﻿// BlueBrick, a LEGO(c) layout editor.
 // Copyright (C) 2008 Alban NANTY
 //
 // This program is free software: you can redistribute it and/or modify
@@ -16,58 +16,50 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using BlueBrick.MapData;
-using System.Drawing;
 
-namespace BlueBrick.Actions.Texts
+namespace BlueBrick.Actions.Rulers
 {
-	class DuplicateText : Items.DuplicateItems
+	class DuplicateRuler : Items.DuplicateItems
 	{
-		private LayerText mTextLayer = null;
+		private LayerRuler mRulerLayer = null;
 
-		public DuplicateText(LayerText layer, List<Layer.LayerItem> itemsToDuplicate, bool needToAddOffset)
+		public DuplicateRuler(LayerRuler layer, List<Layer.LayerItem> itemsToDuplicate, bool needToAddOffset)
 			: base(itemsToDuplicate, needToAddOffset)
 		{
 			// init the layer
-			mTextLayer = layer;
+			mRulerLayer = layer;
 		}
 
 		public override string getName()
 		{
 			if (mItems.Count == 1)
-			{
-				string text = (mItems[0] as LayerText.TextCell).Text.Replace("\r\n", " ");
-				if (text.Length > 10)
-					text = text.Substring(0, 10) + "...";
-				return BlueBrick.Properties.Resources.ActionDuplicateText.Replace("&", text);
-			}
+				return BlueBrick.Properties.Resources.ActionDuplicateRuler;
 			else
-			{
-				return BlueBrick.Properties.Resources.ActionDuplicateSeveralTexts;
-			}
+				return BlueBrick.Properties.Resources.ActionDuplicateSeveralRulers;
 		}
 
 		public override void redo()
 		{
-			// clear the selection first, because we want to select only all the added texts
-			mTextLayer.clearSelection();
-			// add all the texts (by default all the text index are initialized with -1
+			// clear the selection first, because we want to select only all the added rulers
+			mRulerLayer.clearSelection();
+			// add all the rulers (by default all the rulers index are initialized with -1
 			// so the first time they are added, we just add them at the end,
 			// after the index is record in the array during the undo)
-			// We must add all the texts in the reverse order to avoid crash (insert with an index greater than the size of the list)
+			// We must add all the rulers in the reverse order to avoid crash (insert with an index greater than the size of the list)
 			for (int i = mItems.Count - 1; i >= 0; --i)
 			{
-				mTextLayer.addTextCell(mItems[i] as LayerText.TextCell, mItemIndex[i]);
-				mTextLayer.addObjectInSelection(mItems[i]);
+				mRulerLayer.addRulerItem(mItems[i] as LayerRuler.RulerItem, mItemIndex[i]);
+				mRulerLayer.addObjectInSelection(mItems[i]);
 			}
 		}
 
 		public override void undo()
 		{
-			// remove the specified brick from the list of the layer,
+			// remove the specified ruler from the list of the layer,
 			// but do not delete it, also memorise its last position
 			mItemIndex.Clear();
 			foreach (Layer.LayerItem obj in mItems)
-				mItemIndex.Add(mTextLayer.removeTextCell(obj as LayerText.TextCell));
+				mItemIndex.Add(mRulerLayer.removeRulerItem(obj as LayerRuler.RulerItem));
 		}
 	}
 }
