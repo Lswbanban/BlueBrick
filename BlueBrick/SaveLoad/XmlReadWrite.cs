6054bef2094d863a2e5a57f48a1c7eb3d1791d78
Alban Nanty <banban@banban.org>
2018-03-04 14:02:22 +0800

    | refactor again the code that tries to fix duplicated ids in old files: use another hashtable for the duplicated ids

--- BlueBrick/SaveLoad/XmlReadWrite.cs
+++ BlueBrick/SaveLoad/XmlReadWrite.cs
@@ -73,7 +73,7 @@ namespace BlueBrick.MapData
 
         public static SaveLoadManager.UniqueId readItemId(System.Xml.XmlReader reader)
 		{
-            return new SaveLoadManager.UniqueId(reader.ReadElementContentAsString(), false);
+            return new SaveLoadManager.UniqueId(reader.ReadElementContentAsString());
 		}
 
         public static void writeItemId(System.Xml.XmlWriter writer, string name, SaveLoadManager.UniqueId id)
