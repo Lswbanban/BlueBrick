d30f035b8983161070acba4cd3c0bd1b7e8e5538
Alban Nanty <banban@banban.org>
2020-10-27 18:43:33 +0800

    | change the unit of the Brick Elevation Edition in LDU to be compatible with the current altitude saved in the old BBM files

--- BlueBrick/EditBrickForm.Designer.cs
+++ BlueBrick/EditBrickForm.Designer.cs
@@ -34,6 +34,7 @@
 			this.label2 = new System.Windows.Forms.Label();
 			this.okButton = new System.Windows.Forms.Button();
 			this.cancelButton = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
 			((System.ComponentModel.ISupportInitialize)(this.elevationNumericUpDown)).BeginInit();
 			this.SuspendLayout();
 			// 
@@ -44,14 +45,19 @@
 			// 
 			// elevationNumericUpDown
 			// 
+			this.elevationNumericUpDown.Increment = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
 			resources.ApplyResources(this.elevationNumericUpDown, "elevationNumericUpDown");
 			this.elevationNumericUpDown.Maximum = new decimal(new int[] {
-            1000,
+            10000,
             0,
             0,
             0});
 			this.elevationNumericUpDown.Minimum = new decimal(new int[] {
-            1000,
+            10000,
             0,
             0,
             -2147483648});
@@ -64,17 +70,22 @@
 			// 
 			// okButton
 			// 
-			this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
 			resources.ApplyResources(this.okButton, "okButton");
+			this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
 			this.okButton.Name = "okButton";
 			this.okButton.UseVisualStyleBackColor = true;
 			// 
 			// cancelButton
 			// 
-			this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
 			resources.ApplyResources(this.cancelButton, "cancelButton");
+			this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
 			this.cancelButton.Name = "cancelButton";
 			this.cancelButton.UseVisualStyleBackColor = true;
+			// 
+			// label3
+			// 
+			resources.ApplyResources(this.label3, "label3");
+			this.label3.Name = "label3";
 			// 
 			// EditBrickForm
 			// 
@@ -82,6 +93,7 @@
 			resources.ApplyResources(this, "$this");
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.CancelButton = this.cancelButton;
+			this.Controls.Add(this.label3);
 			this.Controls.Add(this.cancelButton);
 			this.Controls.Add(this.okButton);
 			this.Controls.Add(this.label2);
@@ -101,5 +113,6 @@
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Button okButton;
 		private System.Windows.Forms.Button cancelButton;
+		private System.Windows.Forms.Label label3;
 	}
 }
