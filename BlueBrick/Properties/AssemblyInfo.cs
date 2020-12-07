d30674f8e393971b4a977c2221c8d25ebcc09cfb
Alban Nanty <banban@banban.org>
2020-12-07 20:49:06 +0800

    | update the version number to build a beta version

--- BlueBrick/Properties/AssemblyInfo.cs
+++ BlueBrick/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.8.3.8")]
-[assembly: AssemblyFileVersion("1.8.3.8")]
+[assembly: AssemblyVersion("1.8.3.10")]
+[assembly: AssemblyFileVersion("1.8.3.10")]
